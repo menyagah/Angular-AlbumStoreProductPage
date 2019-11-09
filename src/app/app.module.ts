2c064cba009d109d231ddacf92e181f973f57d10
Martin Nyagah <menyagah27@gmail.com>
2019-11-09 22:56:22 +0300

    Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent }
-]
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch:'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
